6feba2defe28bb4658798aadbafbdc96e512c64c
Aliff <aliff@gtriipglobal.com>
2021-03-21 09:13:17 +0800

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by r00t on 21/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
